e8fbf4207cffe955cd6ad0fbe1b7291226104eaa
Chris Berry <chris.bery@getcruise.com>
2020-06-22 15:02:46 -0700

    simpleadd: add host timer

--- simpleadd/simpleadd.cpp
+++ simpleadd/simpleadd.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <algorithm>
+#include <chrono>
 #include <CL/cl.hpp>
  
+using TimeUnit = std::chrono::microseconds;
+using SystemClock = std::chrono::system_clock;
+
 int main(){
     //get all platforms (drivers)
     std::vector<cl::Platform> all_platforms;
@@ -34,11 +39,6 @@ int main(){
     std::stringstream buffer;
     buffer << t.rdbuf();
     std::string kernel_code= buffer.str();
-    /*
-            "   void kernel simple_add(global const int* A, global const int* B, global int* C){       "
-            "       C[get_global_id(0)]=A[get_global_id(0)]+B[get_global_id(0)];                 "
-            "   }                                                                               ";
-    */
     sources.push_back({kernel_code.c_str(),kernel_code.length()});
  
     cl::Program program(context,sources);
@@ -47,7 +47,6 @@ int main(){
         exit(1);
     }
  
- 
     // create buffers on the device
     cl::Buffer buffer_A(context,CL_MEM_READ_WRITE,sizeof(int)*10);
     cl::Buffer buffer_B(context,CL_MEM_READ_WRITE,sizeof(int)*10);
@@ -55,39 +54,45 @@ int main(){
  
     int A[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     int B[] = {0, 1, 2, 0, 1, 2, 0, 1, 2, 0};
+    std::vector<int> C(10);
  
-    //create queue to which we will push commands for the device.
-    cl::CommandQueue queue(context,default_device,CL_QUEUE_PROFILING_ENABLE);
- 
-    //write arrays A and B to the device
-    queue.enqueueWriteBuffer(buffer_A,CL_TRUE,0,sizeof(int)*10,A);
-    queue.enqueueWriteBuffer(buffer_B,CL_TRUE,0,sizeof(int)*10,B);
- 
- 
-    //run the kernel
-    //cl::KernelFunctor simple_add(cl::Kernel(program,"simple_add"),queue,cl::NullRange,cl::NDRange(10),cl::NullRange);
-    //simple_add(buffer_A,buffer_B,buffer_C);
- 
-    //alternative way to run the kernel
     cl::Kernel kernel_add=cl::Kernel(program,"simple_add");
     kernel_add.setArg(0,buffer_A);
     kernel_add.setArg(1,buffer_B);
     kernel_add.setArg(2,buffer_C);
     cl::Event event;
-    queue.enqueueNDRangeKernel(kernel_add,cl::NullRange,cl::NDRange(10),cl::NullRange,NULL,&event);
-    queue.finish();
+
+    //create queue to which we will push commands for the device.
+    cl::CommandQueue queue(context,default_device,CL_QUEUE_PROFILING_ENABLE);
  
+    TimeUnit start_t = std::chrono::duration_cast<TimeUnit>(SystemClock::now().time_since_epoch());
+    {
+        //write arrays A and B to the device, schedule kernel, wait for kernel, read result
+        queue.enqueueWriteBuffer(buffer_A,CL_TRUE,0,sizeof(int)*10,A);
+        queue.enqueueWriteBuffer(buffer_B,CL_TRUE,0,sizeof(int)*10,B);
+        queue.enqueueNDRangeKernel(kernel_add,cl::NullRange,cl::NDRange(10),cl::NullRange,NULL,&event);
+        queue.finish();
+        queue.enqueueReadBuffer(buffer_C,CL_TRUE,0,sizeof(int)*10,C.data());
+    }
+    TimeUnit end_t = std::chrono::duration_cast<TimeUnit>(SystemClock::now().time_since_epoch());
+        
+ 
+    std::vector<int> result({0,2,4,3,5,7,6,8,10,9});
+    if (std::equal(result.begin(),result.end(),C.begin())) {
+        std::cout << "Simpleadd PASSED | ";
+    } else {
+        std::cout << "Simpleadd FAILED | ";
+    }
     auto start = event.getProfilingInfo<CL_PROFILING_COMMAND_START>();
     auto end = event.getProfilingInfo<CL_PROFILING_COMMAND_END>();
-    std::cout << "Elapsed time: " << (end-start) << " ns." << std::endl;
+    std::cout << "GPU: " <<(end-start) << "ns | ";
+    std::cout << "Total: " << std::chrono::duration_cast<TimeUnit>(end_t - start_t).count()
+              << " µs." << std::endl;
 
-    int C[10];
-    //read result C from the device to array C
-    queue.enqueueReadBuffer(buffer_C,CL_TRUE,0,sizeof(int)*10,C);
- 
+
     std::cout<<" result: \n";
-    for(int i=0;i<10;i++){
-        std::cout<<C[i]<<" ";
+    for(const auto &elem : C){
+        std::cout<<elem<<" ";
     }
     std::cout<<std::endl;
  
